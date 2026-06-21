18019a660b8b5eeed33308d5394f17bc3a7b236d
pinakigitub <pinaki.best@gmail.com>
2026-06-21 18:13:31 +0530

    sdcdsf

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+sqs = boto3.client("sqs", region_name="ap-south-1")
+
+QUEUE_URL = os.getenv("QUEUE_URL")
 
 @app.get("/")
 def home():
     return {
         "message":"FastAPI running on ECS alb PINAKI 10"
+    }
+
+@app.post("/order")
+def create_order(order: dict):
+    response = sqs.send_message(
+        QueueUrl=QUEUE_URL,
+        MessageBody=json.dumps(order)
+    )
+
+    return {
+        "message": "Order queued",
+        "message_id": response["MessageId"]
     }
