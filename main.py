189ac036286cb3a99c96f417979ad95c99a4d59e
pinakigitub <pinaki.best@gmail.com>
2026-06-21 18:50:57 +0530

    sdcdsf

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ QUEUE_URL = os.getenv("QUEUE_URL")
 @app.get("/")
 def home():
     return {
-        "message":"FastAPI running on ECS alb PINAKI 10"
+        "message":"FastAPI running on ECS alb PINAKI 101"
     }
 
 @app.post("/order")
